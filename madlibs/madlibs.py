63bc124ef91b79f1356e6d055b846706aa33529f
priya-mandyal <priya.mandyal98@gmail.com>
2024-04-25 20:31:28 -0300

    Beginner project

---
+++ madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective")
+verb1 = input("Verb")
+verb2 = input("Verb")
+famous_name = input("Famous person")
+
+madlib = f"Coding is very {adj}. If you like to {verb1} then you will enjoy it. Stay hydrated and {verb2} \
+like you are {famous_name}"
+
+print(madlib)
